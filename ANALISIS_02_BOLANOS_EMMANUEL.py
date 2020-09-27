6a8c9a1a7eab1082523d28d1642d2db284b1aa4c
Emmanuel Bolanhos <eboufc@gmail.com>
2020-09-27 15:24:31 -0500

    working v1.0

--- ANALISIS_02_BOLANOS_EMMANUEL.py
+++ ANALISIS_02_BOLANOS_EMMANUEL.py
@@ -1,3 +1,5 @@
+# Agrega menu + guarda en csv
+# GITHUB EN DOC!
 # Importamos las librerías a usar
 import pandas as pd
 
@@ -6,37 +8,61 @@ import pandas as pd
 with open("synergy_logistics_database.csv", "r") as database:
     df = pd.read_csv(database, index_col=0)
 
-# OP.1 RUTAS IN/OUT. Deberíamos enfocarnos en las 10 rutas mas demandadas.
+# OP.1 RUTAS IN/OUT. Cuales son las 10 rutas con más flujo
 # Obtenemos los nombres de las columnas
 # y los asignamos a una variable
 cols = df.columns
-# print(cols)
 
 # Tomamos las columnas que nos interesan para realizar
 # el análisis propuesto en la opción 1
-util_cols_1 = [cols[0], cols[1], cols[2]]
+util_cols_1 = [cols[0], cols[1], cols[2], cols[8]]
 
 # Filtramos la df para solo mostrar las columnas
 # que nos interesan
 df_op1 = df.loc[:, util_cols_1]
 
 # Agrupamos con groupby las filas donde la columna de dirección,
-# origen y destino coinciden. Además, usando la función de agregado
-# size() calculamos el tamaño de cada grupo (que sería la cantidad
-# de veces que cada ruta es usada) y lo agregamos como una serie.
-# Reiniciamos el índice para mostrar cada fila por separado y asignamos
-# el nombre 'count' a la columna creada por size()
-df_op1 = df_op1.groupby(util_cols_1).size().reset_index(name='count')
+# origen y destino coinciden. Además, usando las funciones sum, count y mean
+# calculamos el total de ganancias, la cantidad de veces que se ha usado la
+# ruta y el promedio lo usamos para conocer cual es la ganancia por viaje
+df_op1 = df_op1.groupby([cols[0], cols[1], cols[2]]).\
+    agg(['sum', 'count', 'mean']).reset_index()
 
-# Ordenamos los valores en orden descendente. El argumento inplace
-# indica que el cambio toma lugar en la misma variable
-df_op1.sort_values('count', ascending=False, inplace=True)
+# Calculamos la ganancia por viaje promedio. Este dato nos ayudará a filtrar
+# las rutas que no produzcan suficientes ganancias
+avg_mean = df_op1.loc[:, ('total_value', 'sum')].sum() / \
+           df_op1.loc[:, ('total_value', 'count')].sum()
+
+# Quitamos las rutas que no cumplan con el mínimo de ganancias
+df_op1 = (df_op1.loc[df_op1.loc[:, ('total_value', 'mean')] > avg_mean])
+
+# Ordenamos los valores en orden descendente, para conocer las rutas
+# que se usaron más de la tabla filtrada
+df_op1 = df_op1.sort_values([('total_value', 'count')], ascending=False)
+
+# Creamos una dataframe para exportaciones y otra para importaciones
+df_op1_exp = df_op1.loc[df_op1.iloc[:, 0] == 'Exports']
+df_op1_imp = df_op1.loc[df_op1.iloc[:, 0] == 'Imports']
 
 # Usamos slicing para seleccionar solo las primeras 10 rutas
-df_ans1 = df_op1[:10]
+df_op1_exp_ans = df_op1_exp.iloc[:10, 1:]
+df_op1_imp_ans = df_op1_imp.iloc[:10, 1:]
+
+# Además, observamos las rutas menos usadas
+df_op1_exp = df_op1_exp.iloc[-10:, 1:]
+df_op1_imp = df_op1_imp.iloc[-10:, 1:]
 
 # Mostramos el resultado
-# print(df_ans1)
+print('\n\nRutas de exportación más usadas\n')
+print(df_op1_exp_ans)
+print('\nRutas de importación más usadas\n')
+print(df_op1_imp_ans)
+
+# Mostramos las rutas menos usadas
+print('\n\nRutas de exportación menos usadas\n')
+print(df_op1_exp)
+print('\nRutas de importación menos usadas\n')
+print(df_op1_imp)
 
 # OP.2 MEDIO DE TRANSPORTE. Los 3 medios mas importantes según el valor
 # de las in/out. Cual se puede reducir.
@@ -70,9 +96,11 @@ max_rets = df_op2.iloc[0:4, 1:4]/max_by_column
 max_rets = max_rets.rename(
     columns={'sum': 'norm_sum', 'count': 'norm_count', 'mean': 'norm_mean'})
 
-# Agregamos los valores obtenidos a nuestra dataframe anterior e imprimimos
+# Agregamos los valores obtenidos a nuestra dataframe anterior
+# e imprimimos el resultado
 df_op2 = df_op2.join(max_rets)
-# print(df_op2)
+print('\n\nGanancias por medio de transporte\n')
+print(df_op2)
 
 # OP.3 VALOR TOTAL DE IN/OUT. Si nos enfocamos en los países que generan
 # el 80% del valor de los in/outs, que países serian esos.
@@ -93,17 +121,25 @@ countries_per_export = (countries_sum_exports.iloc[:, 2]
 countries_per_import = (countries_sum_imports.iloc[:, 2]
                         / countries_sum_imports.iloc[:, 2].sum()).rename('total_%')
 
+# Agregamos los porcentajes obtenidos a su respectivo dataframe
 countries_sum_exports = pd.concat([countries_sum_exports, countries_per_export], axis=1)
 countries_sum_imports = pd.concat([countries_sum_imports, countries_per_import], axis=1)
 
+# Ordenamos ambos dataframes en orden descendente
 countries_sum_exports.sort_values(['total_%'], ascending=False, inplace=True)
 countries_sum_imports.sort_values(['total_%'], ascending=False, inplace=True)
 
+# Hacemos una suma acumulativa, de esta forma conoceremos los países
+# que generan el 80% de los ingresos
 countries_sum_exports['cumulative_%'] = countries_sum_exports['total_%'].cumsum()
 countries_sum_imports['cumulative_%'] = countries_sum_imports['total_%'].cumsum()
 
+# Filtramos las datasets para mostrar solo las que están dentro del limite
 countries_sum_exports = countries_sum_exports.loc[countries_sum_exports['cumulative_%'] <= 0.8]
 countries_sum_imports = countries_sum_imports.loc[countries_sum_imports['cumulative_%'] <= 0.8]
 
-# print(countries_sum_exports)
-# print(countries_sum_imports)
+# Mostramos el resultado
+print('\n\n Lista de países exportadores que aportan al 80% de ganancias\n')
+print(countries_sum_exports)
+print('\n Lista de países importadores que aportan al 80% de ganancias\n')
+print(countries_sum_imports)
